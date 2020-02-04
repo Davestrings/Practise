fc3561c06fa1d80e9083cb302c995102da2e358f
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-02-03 16:26:16 -0800

    program convert temperature in celsius or fahrenheit

---
+++ TemperatureConverter.java
@@ -0,0 +1,39 @@
+package Practise;
+import java.util.Scanner;
+public class TemperatureConverter {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter 1 for celsius & 2 for fahrenheit: ");
+		int choice = scan.nextInt();
+		System.out.print("Enter temperature: ");
+		double number = scan.nextDouble();
+		
+		displayTemperature(choice, number);
+		
+		scan.close();
+	}
+	
+	
+	public static double convertCelsius( double fahrenheit ) {
+		double celsius = 5.0 / 9.0 *(fahrenheit - 32);
+		return celsius;
+	}
+	
+	public static double convertFahrenheit(double celsius) {
+		double fahrenheit = 9.0 / 5.0 * (celsius + 32);
+		return fahrenheit;
+	}
+	
+	public static void displayTemperature(int choice, double number ) {
+		switch(choice) {
+		case 1:
+			System.out.printf("Temperature in Celsius is %.3f",convertCelsius(number));
+			break;
+		case 2:
+			System.out.printf("Temperature in Fahrenheit  is %.3f",convertFahrenheit(number));
+			break;
+		}
+	}
+}
